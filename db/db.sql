0ed27096f732583a95bf25a1bb8e2d38f3840df6
Shane Bramble-Wade <sbramwade@msn.com>
2021-09-26 12:43:41 -0500

    created a user table with sequelize by using models

---
+++ db/db.sql
@@ -0,0 +1,2 @@
+DROP DATABASE IF EXISTS price_tracker;
+CREATE DATABASE price_tracker;
---
+++ server.js
@@ -0,0 +1,12 @@
+
+// TODO:
+
+// The steps we followed in this lesson are the same ones developers take regularly:
+
+// Model the data.
+
+// Set up API routes to work with that data.
+
+// Test the routes to make sure everything works as intended.
+
+// Repeat with new model.
