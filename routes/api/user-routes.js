0aebaea768b62e1e1ffdb1e5556521571d7217cc
Shane Bramble-Wade <sbramwade@msn.com>
2021-09-27 18:41:10 -0500

    implemented login authentication via a POST request

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,12 +1,14 @@
 const router = require('express').Router();
-const {User} = require('../../models');
+const {
+    User
+} = require('../../models');
 
 
 // Retrieve all users
 router.get('/', (req, res) => {
     // MySQL equivalent to -> SELECT * FROM users;
     User.findAll()
-        .then (data => res.json(data))
+        .then(data => res.json(data))
         .catch(err => {
             console.log(err);
             res.status(500).json(err);
@@ -14,35 +16,75 @@ router.get('/', (req, res) => {
 });
 
 // Retrieve a single user.
-router.get ('/:id', (req, res) => {
+router.get('/:id', (req, res) => {
     User.findOne({
-        where: {id: req.params.id}
-    })
-    .then (data => {
-        if (!data){
-            res.status(404).json({message: 'No user found with this id'});
+            where: {
+                id: req.params.id
+            }
+        })
+        .then(data => {
+            if (!data) {
+                res.status(404).json({
+                    message: 'No user found with this id'
+                });
+                return;
+            }
+            res.json(data);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+router.post('/', (req, res) => {
+    User.create({
+            first_name: req.body.first_name,
+            last_name: req.body.last_name,
+            email: req.body.email,
+            password: req.body.password
+        })
+        .then(data => res.json(data))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+router.post('/login', (req, res) => {
+    User.findOne({
+        where: {
+            email: req.body.email
+        }
+    }).then(dbUserData => {
+        if (!dbUserData) {
+            res.status(400).json({
+                message: 'No user with that email address!'
+            });
             return;
         }
-        res.json(data);
-    })
-    .catch (err => {
-        console.log(err);
-        res.status(500).json(err);
+
+        //  res.json({
+        //      user: dbUserData
+        //  });
+
+        // Verify user
+        const validPassword = dbUserData.checkPassword(req.body.password);
+
+        if (!validPassword) {
+            res.status(400).json({
+                message: 'Incorrect password!'
+            });
+            return;
+        }
+
+        res.json({
+            user: dbUserData,
+            message: 'You are now logged in!'
+        });
+
     });
 });
 
-router.post ('/', (req, res) => {
-    User.create({
-        first_name: req.body.first_name,
-        last_name: req.body.last_name,
-        email: req.body.email,
-        password: req.body.password
-    })
-    .then (data => res.json(data))
-    .catch (err =>  {
-        console.log(err);
-        res.status(500).json(err);
-    });
-});
 
 module.exports = router;
