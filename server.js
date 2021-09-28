3c2f694ff310188d9fad0b6377c8d0a8c7eab0b3
Shane Bramble-Wade <sbramwade@msn.com>
2021-09-27 19:35:19 -0500

    added the handlebars.js template

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const routes = require('./routes');
 const sequelize = require('./config/connection');
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
 
 const app = express();
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
