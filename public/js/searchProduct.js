7f61b9b08a0db62ca43f1161f168171c6eb195d3
gabriel-perez1 <83041381+gabriel-perez1@users.noreply.github.com>
2021-10-04 00:32:46 -0400

    scrape product info

---
+++ public/js/searchProduct.js
@@ -0,0 +1,20 @@
+const {searchAmazon, AmazonSearchResult} = require('unofficial-amazon-search');
+
+async function searchHandler(event) {
+  event.preventDefault();
+  // Extract data from search bar
+  const searchInput = document.querySelector('#searchInfo').value.trim();
+
+  searchAmazon(searchInput, {includeSponsoredResults: true}).then(data => {
+    console.log(data.searchResults[0].title)
+    console.log(data.searchResults[0].imageUr)
+    console.log(data.searchResults[0].prices);
+    var productTitle = data.searchResults[0].title;
+    var productImage = data.searchResults[0].imageUrl;
+    var productPrice = data.searchResults[0].prices;
+  })
+};
+
+document.querySelector('#searchForm').addEventListener('submit', searchHandler);
+
+
--- routes/index.js
+++ routes/index.js
@@ -5,12 +5,14 @@ const homeRoutes = require('./html/home-routes');
 const searchRoutes = require('./html/price-notification')
 const loginRoutes = require('./html/login-route');
 const signupRoutes =require('./html/signup-route');
+const productRoute = require('./html/product-info')
 
 router.use('/api', apiRoutes);
 router.use('/', homeRoutes);
 router.use('/', searchRoutes);
 router.use('/', loginRoutes);
 router.use('/', signupRoutes);
+router.use('/', productRoute);
 
 router.use((req, res) => {
     res.status(404).end();
